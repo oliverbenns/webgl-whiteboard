18a6371a7f0244a8099f831da9b9459bc43a73b6
Oliver <oliverbenns@gmail.com>
2019-01-03 00:22:08 +0000

    Fix optional type

--- src/dot.ts
+++ src/dot.ts
@@ -2,17 +2,17 @@ import Color from './color';
 import Vector from './vector';
 
 // @TODO: use 'pick' type here.
-interface DotOptions {
-  color?: Color;
-  scale?: number;
-}
+type DotOptions = Partial<{
+  color: Color;
+  scale: number;
+}>
 
 export default class Dot {
   public color: Color;
   public scale: number;
   public position: Vector;
 
-  constructor(options?: DotOptions = {}) {
+  constructor(options: DotOptions = {}) {
     this.color = new Color();
     this.scale = 5;
     this.position = new Vector();
