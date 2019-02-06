9656ba85a4e6be671896fbd4a28f16727b257775
Oliver <oliverbenns@gmail.com>
2019-02-06 23:40:07 +0000

    Remove keyboard subscriber

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import Color from "./color";
 import Renderer from "./renderer";
 import World from "./world";
 import Pointer from "./pointer";
-import Keyboard from "./keyboard";
 import context from "./context";
 
 const world = new World();
@@ -14,6 +13,5 @@ renderer.setColorsAttributePointer();
 
 const render = () => renderer.render(world);
 
-Keyboard.subscribe("keypress", render);
 Pointer.subscribe("down", render);
 Pointer.subscribe("drag", render);
